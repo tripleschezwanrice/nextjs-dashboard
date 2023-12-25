b46bafdf9dc5a60e4efa94952f060cc166f2a3d2
tripleschezwanrice <varunparmar@jklu.edu.in>
2023-12-25 15:15:34 +0530

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CustomerPage = () => {
+  return (
+    <p>Customer Page</p>
+  )
+}
+
+export default CustomerPage
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InvoicesPage = () => {
+  return (
+    <p>Invoices Page</p>
+  )
+}
+
+export default InvoicesPage
